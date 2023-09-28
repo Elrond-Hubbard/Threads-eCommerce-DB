3cd7fd28d6f8dcc18df06db0be7af5ad347bc44f
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-09-28 16:16:52 -0400

    formatting, comments, cleanup

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,43 +1,41 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
-  Category.findAll({include: [Product]})
+  Category.findAll({ include: [Product] })
     .then((data) => res.json(data))
-    .catch((err) => console.log(err))
-  // be sure to include its associated Products
+    .catch((err) => console.log(err));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   Category.findOne({
-    where: {id: req.params.id},
-    include: [Product]
+    where: { id: req.params.id },
+    include: [Product],
   })
-  .then((data) => res.json(data))
-  .catch((err) => console.log(err))
-  // be sure to include its associated Products
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Category.create(req.body)
     .then((data) => res.json(data))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 });
 
-router.put('/:id', (req, res) => {
-  Category.update(req.body, {where: {id: req.params.id}})
-  .then((data) => res.json(data))
-  .catch((err) => console.log(err))
+router.put("/:id", (req, res) => {
+  Category.update(req.body, { where: { id: req.params.id } })
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
-router.delete('/:id', (req, res) => {
-  Category.destroy({where: {id: req.params.id}})
-  .then(() => console.log('Category deleted!'))
-  .catch((err) => console.log(err))
+router.delete("/:id", (req, res) => {
+  Category.destroy({ where: { id: req.params.id } })
+    .then(() => console.log("Category deleted!"))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,43 +1,41 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
-  Tag.findAll({include: [Product]})
-  .then((data) => res.json(data))
-  .catch((err) => console.log(err))
-  // be sure to include its associated Product data
+  Tag.findAll({ include: [Product] })
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   Tag.findOne({
-    where: {id: req.params.id},
-    include: [Product]
+    where: { id: req.params.id },
+    include: [Product],
   })
-  .then((data) => res.send(data))
-  .catch((err) => console.log(err))
-  // be sure to include its associated Product data
+    .then((data) => res.send(data))
+    .catch((err) => console.log(err));
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Tag.create(req.body)
-  .then((data) => res.json(data))
-  .catch((err) => console.log(err))
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
-router.put('/:id', (req, res) => {
-  Tag.update(req.body, {where: {id: req.params.id}})
-  .then((data) => res.json(data))
-  .catch((err) => console.log(err))
+router.put("/:id", (req, res) => {
+  Tag.update(req.body, { where: { id: req.params.id } })
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
-router.delete('/:id', (req, res) => {
-  Tag.destroy({where: {id: req.params.id}})
-  .then(() => console.log('Tag deleted!'))
-  .catch((err) => console.log(err))
+router.delete("/:id", (req, res) => {
+  Tag.destroy({ where: { id: req.params.id } })
+    .then(() => console.log("Tag deleted!"))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
